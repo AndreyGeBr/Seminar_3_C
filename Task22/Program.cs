b13139b2587cb1da1dcc4a36ff6d471e552d70f2
Andrey <a89615076005@yandex.ru>
2022-09-25 19:53:21 +0300

    add task 22

---
+++ Task22/Program.cs
@@ -0,0 +1,25 @@
+﻿// Программа, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+Console.Clear();
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+/* int count = 1;
+double sqw;
+
+while (count <= n)
+{
+    sqw = Math.Pow(count, 2);
+    Console.WriteLine($"{count} в квадрате равно {sqw} ");
+    count++;
+} */
+
+int count = 1;
+while (count <= n)
+{
+    Console.Write($"{Math.Pow(count, 2)}");
+    if (count != n)
+        Console.Write(", ");
+    count++;
+}
+

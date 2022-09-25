f40484376d86e14eee39ca556de0d3c686efcc21
Andrey <a89615076005@yandex.ru>
2022-09-25 18:12:38 +0300

    dd task 18

---
+++ Task18/Program.cs
@@ -0,0 +1,26 @@
+﻿// Программа, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+Console.Clear();
+
+Console.Write("Введите номер четверти: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number == 1)
+{
+    Console.WriteLine("Диапазон возможных координат: x > 0 и y > 0");
+}
+else if (number == 2)
+{
+    Console.WriteLine("Диапазон возможных координат: x < 0 и y > 0");
+}
+else if (number == 3)
+{
+    Console.WriteLine("Диапазон возможных координат: x < 0 и y < 0 ");
+}
+else if (number == 4)
+{
+    Console.WriteLine("Диапазон возможных координат: x > 0 и y < 0");
+}
+else 
+{
+    Console.WriteLine("Есть всего 4 четверти. Введите число от 1 до 4");    
+}
